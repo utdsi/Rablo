6282960da649e400539cedec9c2ab736824d9bea
Utkarsh SInha <vishalsingh@Vishals-MacBook-Air.local>
2024-01-12 17:41:59 +0530

    updated with cors

--- Backend/index.js
+++ Backend/index.js
@@ -3,8 +3,10 @@
 const express = require("express")
 
 const app = express()
+const cors = require("cors")
 
 app.use(express.json())
+app.use(cors())
 
 
 const {connection}  = require("./config/db")
